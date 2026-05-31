9416d019fa01c56d44b87c15a1c12b4d1a1efcf3
Franklin Okech <franklinokech@gmail.com>
2026-05-31 11:21:42 +0300

    feat: dockerised the maternal script

--- run_dqa.sh
+++ run_dqa.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# run_dqa.sh - Shell wrapper for data clerk launcher
-
-# Get the directory where this script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$SCRIPT_DIR"
-
-# Run the R script
-Rscript run_dqa_launcher.R
-
-# Keep terminal open on error
-if [ $? -ne 0 ]; then
-    echo ""
-    echo "An error occurred. Press Enter to close..."
-    read
-fi
+cd ~/Desktop/redcap_dqa
+docker compose up --abort-on-container-exit
+xdg-open cin_maternal/data/output
+read -p "Press Enter to close..."
